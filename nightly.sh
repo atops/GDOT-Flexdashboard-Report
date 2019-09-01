fb65e842c2fd638a38ccbcb93dfd9895247fe123
atops <alan.toppen@gmail.com>
2019-09-01 20:22:33 +0000

    updates for logrotate

--- nightly.sh
+++ nightly.sh
@@ -2,7 +2,7 @@
 
 H=$(TZ=America/New_York date +%H)
 
-echo "This always runs. The hour is ${H#0}"
+echo "System booted. The hour is ${H#0}"
 
 # Only start the MARK 1 calcs and package scripts on startup if it's early morning
 # i.e., it is being started by the script on The SAM after loading raw data into S3
@@ -17,6 +17,10 @@ if [[ ${H#0} -lt 6 ]]; then
     echo $(TZ=America/New_York date)
     Rscript Monthly_Report_Calcs_ec2.R
     Rscript Monthly_Report_Package.R
+    
+    # this was moved to crontab to run at midnight. creates empty nightly.log.
+    #/usr/sbin/logrotate /home/rstudio/logrotate_nightly.conf --state=/home/rstudio/logrotate-state --verbose
+    aws s3 sync /home/rstudio/ s3://gdot-spm/logs --exclude "*" --include "nightly.lo*"
 else
     echo "$(TZ=America/New_York date) - after 6am"
 fi
