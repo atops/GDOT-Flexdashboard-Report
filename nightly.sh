36c09899a54cec159db42cb6a7a2b03c5b721450
atops <alan.toppen@gmail.com>
2019-10-07 15:44:53 -0400

    Comment out poweroff

--- nightly.sh
+++ nightly.sh
@@ -22,7 +22,7 @@ if [[ ${H#0} -lt 6 ]]; then
     #/usr/sbin/logrotate /home/rstudio/logrotate_nightly.conf --state=/home/rstudio/logrotate-state --verbose
     aws s3 sync /home/rstudio/ s3://gdot-spm/logs --exclude "*" --include "nightly.lo*"
     
-    systemctl poweroff -i
+    #systemctl poweroff -i
 else
     echo "$(TZ=America/New_York date) - after 6am"
 fi
