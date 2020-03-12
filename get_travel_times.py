615c640307e5468daa20c4ca31985fc0a31fd00a
atops <alan.toppen@gmail.com>
2020-03-12 15:47:40 +0000

    Check for success before pulling data, explicitly read Readings.csv from downloaded zip file, split tmcs into groups of 1000 for query and bind at the end.

--- get_travel_times.py
+++ get_travel_times.py
@@ -8,15 +8,16 @@ Created on Mon Sep 24 20:42:51 2018
 
 #import os
 import pandas as pd
+import numpy as np
 import requests
 import uuid
 import polling
 import time
 import yaml
 from datetime import datetime, timedelta
-#import zipfile
+from zipfile import ZipFile
+import json
 import io
-#import feather
 import boto3
 
 #os.environ['TZ'] = 'America/New_York'
@@ -89,21 +90,26 @@ def get_tmc_data(start_date, end_date, tmcs, key, initial_sleep_sec=0):
     if response.status_code == 200: # Only if successful response
 
         # retry at intervals of 'step' until results return (status code = 200)
-        results = polling.poll(
-                lambda: requests.get(uri.format('jobs/export/results'), 
-                               params = {'key': key, 'uuid': payload['uuid']}),
-                check_success = lambda x: x.status_code == 200 and len(x.content) > 0,
-                step = 1,
-                timeout = 300
-                #poll_forever = True
-                )
+        jobid = json.loads(response.content.decode('utf-8'))['id']
+
+        polling.poll(
+            lambda: requests.get(uri.format('jobs/status'), params = {'key': key, 'jobId': jobid}),
+            check_success = lambda x: json.loads(x.content.decode('utf-8'))['state']=='SUCCEEDED',
+            step=1,
+            timeout=600)
+
+        results = requests.get(uri.format('jobs/export/results'), 
+                               params = {'key': key, 'uuid': payload['uuid']})
         print('results received')
         
         # Save results (binary zip file with one csv)
         with io.BytesIO() as f:
             f.write(results.content)
             f.seek(0)
-            df = pd.read_csv(f, compression='zip')       
+            #df = pd.read_csv(f, compression='zip')       
+            with ZipFile(f, 'r') as zf:
+                df = pd.read_csv(zf.open('Readings.csv'))
+                #tmci = pd.read_csv(zf.open('TMC_Identification.csv'))
 
     else:
         df = pd.DataFrame()
@@ -186,8 +192,14 @@ if __name__=='__main__':
     print(start_date)
     print(end_date)
 
+    group_size = 1000
+    tmc_groups = np.split(tmc_list, range(group_size, len(tmc_list), group_size))
+
     try:
         tt_df = get_tmc_data(start_date, end_date, tmc_list, cred['RITIS_KEY'], 0)
+        # tt_df = pd.concat(
+        #     [get_tmc_data(start_date, end_date, list(tmc_group), cred['RITIS_KEY'], 1) for tmc_group in tmc_groups]
+        # )
 
     except Exception as e:
         print('ERROR retrieving records')
