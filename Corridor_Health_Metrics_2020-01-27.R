bfb50f3dd476141332a7277325c2b49375960099
atops <atops@users.noreply.github.com>
2020-06-10 18:19:34 +0100

    Includes ped delay, cctv, progression ratio (omitted previously)

--- Corridor_Health_Metrics_2020-01-27.R
+++ Corridor_Health_Metrics_2020-01-27.R
@@ -17,11 +17,14 @@ get_summary_data <- function(df, current_month = NULL) {
     data <- list(
         rename(df$mo$du, du = uptime, du.delta = delta),
         rename(df$mo$pau, pau = uptime, pau.delta = delta),
-        #rename(df$mo$cctv, cctv = uptime, cctv.delta = delta),
+        rename(df$mo$pdc, pdc = Avg.Max.Ped.Delay, pdc.delta = delta),
+        rename(df$mo$pdf, pdf = Avg.Max.Ped.Delay, pdf.delta = delta),
+        rename(df$mo$cctv, cctv = uptime, cctv.delta = delta),
         rename(df$mo$ru, rsu = uptime, rsu.delta = delta),
         rename(df$mo$cu, cu = uptime, cu.delta = delta),
         rename(df$mo$tp, tp.delta = delta),
         rename(df$mo$aogd, aog.delta = delta),
+        rename(df$mo$prd, pr.delta = delta),
         rename(df$mo$qsd, qs.delta = delta),
         rename(df$mo$sfd, sf.delta = delta),
         rename(df$mo$tti, tti.delta = delta),
@@ -29,7 +32,7 @@ get_summary_data <- function(df, current_month = NULL) {
     ) %>%
         reduce(full_join, by = c("Zone_Group", "Corridor", "Month")) %>%
         filter(
-            Zone_Group != Corridor
+            as.character(Zone_Group) != as.character(Corridor)
         ) %>%
         select(
             -uptime.sb,
@@ -39,7 +42,8 @@ get_summary_data <- function(df, current_month = NULL) {
             -starts_with("ones"),
             -starts_with("cycles"),
             -starts_with("pct"),
-            -starts_with("vol")
+            -starts_with("vol"),
+            -starts_with("num")
         )
     if ("mttr" %in% names(df$mo)) { # cor, not sub
         data <- data %>% 
@@ -60,13 +64,36 @@ get_summary_data <- function(df, current_month = NULL) {
 }
 
 
+source("Monthly_Report_Package_init.R")
+options(dplyr.summarise.inform = FALSE)
+
+# Ped delay - coordinated and free signals, csv from Anthony
+pdc <- read_csv("health_ped_delay_data/ped_delay_summary_coordinated_Q12020_monthly.csv") %>% 
+    mutate(
+        SignalID = factor(SignalID),
+        Month = ymd(glue("2020-{Month}-01")))
+pdf <- read_csv("health_ped_delay_data/ped_delay_summary_free_Q12020_monthly.csv") %>% 
+    mutate(
+        SignalID = factor(SignalID),
+        Month = ymd(glue("2020-{Month}-01")))
+
+sub_mo_pdc <- get_cor_monthly_avg_by_day(pdc, subcorridors, "Avg.Max.Ped.Delay")
+sub_mo_pdf <- get_cor_monthly_avg_by_day(pdf, subcorridors, "Avg.Max.Ped.Delay")
+
 sub <- s3readRDS(bucket = "gdot-spm", object = "sub_ec2.rds")
 cor <- s3readRDS(bucket = "gdot-spm", object = "cor_ec2.rds")
+
+sub$mo$pdc <- sub_mo_pdc
+sub$mo$pdf <- sub_mo_pdf
 
 current_month <- ymd("2020-04-01")
 
 csd <- get_summary_data(sub)
 cmd <- get_summary_data(cor)
+
+
+
+
 
 
 lapply(cor$mo, names)
@@ -83,4 +110,4 @@ lapply(sub$dy, names)
 
 
                                     
-write_csv(csd, "health_metrics_data_2020-05-18.csv")
+write_csv(csd, "health_metrics_data_2020-06-03.csv")
