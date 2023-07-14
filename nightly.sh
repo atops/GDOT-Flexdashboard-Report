3be141d778ba755dca662fb6dfe1409e0575941d
atops <alan.toppen>
2023-07-14 13:51:28 +0000

    add --no-follow-symlinks to avoid warnings

--- nightly.sh
+++ nightly.sh
@@ -47,7 +47,7 @@ if [[ ${H#0} -lt 6 ]]; then
         python user_delay_costs.py
     fi
 
-    aws s3 sync /home/rstudio/ s3://$bucket/logs --exclude "*" --include "nightly.lo*" --region us-east-1
+    aws s3 sync /home/rstudio/ s3://$bucket/logs --exclude "*" --include "nightly.lo*" --region us-east-1 --no-follow-symlinks
     
     # Shut down when script completes
     aws ec2 stop-instances --instance-ids i-0ddfe60da0c6fe4bd
