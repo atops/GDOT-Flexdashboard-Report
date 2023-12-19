f5266e6507c4e0b36b592f00ea3ee42f4b35d85f
atops <alan.toppen>
2023-12-19 15:58:25 +0000

    Update shell scripts to call sigops conda environment.

--- etl_dashboard.sh
+++ etl_dashboard.sh
@@ -1,2 +1,2 @@
-~/miniconda3/bin/python etl_dashboard.py $1 $2
+~/miniconda3/bin/conda run -n sigops python etl_dashboard.py $1 $2
 
--- get_aog.sh
+++ get_aog.sh
@@ -1,2 +1,2 @@
-~/miniconda3/bin/python get_aog.py $1 $2
+~/miniconda3/bin/conda run -n sigops python get_aog.py $1 $2
 
